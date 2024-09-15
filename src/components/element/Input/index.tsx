265d4094a6bde7d5225f5529c43b8c0efa5a158f
anwarhakim31 <anwarhakim31@gmail.com>
2024-09-15 09:15:10 +0700

    Fix case sensitivity issues in filenames

---
+++ src/components/element/Input/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./input.module.scss";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const InputElement = ({ type, placeholder, name, id, field }: any) => {
+  return (
+    <input
+      style={{
+        paddingRight: name === "password" && "2.5rem",
+      }}
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      id={id}
+      {...field}
+      className={styles.input}
+    />
+  );
+};
+
+export default InputElement;
