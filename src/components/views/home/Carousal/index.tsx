34f5ea3e32427ec0eb7c0c6ad85789c2f4f35434
anwarhakim31 <anwarhakim31@gmail.com>
2024-09-22 22:41:21 +0700

    feat:rebuild carousal

--- src/components/views/home/Carousal/index.tsx
+++ src/components/views/home/Carousal/index.tsx
@@ -5,79 +5,78 @@ import styles from "./coraousal.module.scss";
 import Image from "next/image";
 
 const Caraousal = () => {
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <div className={styles.container} ref={containerRef}>
-      <div className={styles.container__wrapper} ref={wrapperRef}>
-        <Image
-          src={"/marqueen/1.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/2.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/3.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/4.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-      </div>
-      <div className={styles.container__wrapper} ref={wrapperRef}>
-        <Image
-          src={"/marqueen/1.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/2.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/3.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-        <Image
-          src={"/marqueen/4.png"}
-          alt="logo"
-          height={250}
-          width={250}
-          priority
-          className={styles.logo}
-        />
-      </div>
+      {Array.from({ length: 2 }).map((_, index) => (
+        <div key={index} className={styles.container__wrapper}>
+          <Image
+            src={"/marqueen/1.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/2.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/3.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/4.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/1.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/2.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/3.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+          <Image
+            src={"/marqueen/4.png"}
+            alt="logo"
+            height={250}
+            width={250}
+            priority
+            className={styles.logo}
+          />
+        </div>
+      ))}
     </div>
   );
 };
